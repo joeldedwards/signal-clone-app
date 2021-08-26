9cae17baa3c60541588817be8d6471ad3d32ee22
Joel Edwards <joeldedwards@users.noreply.github.com>
2021-08-25 23:40:07 -0400

    Login Screen added.

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB81-HLtRqu9pyixuY5er5XxYAm4w_pGBo",
+    authDomain: "signal-clone-app-d4e29.firebaseapp.com",
+    projectId: "signal-clone-app-d4e29",
+    storageBucket: "signal-clone-app-d4e29.appspot.com",
+    messagingSenderId: "1091300243716",
+    appId: "1:1091300243716:web:b59656a618587a83faf851"
+  };
